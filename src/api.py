0e2d4280121aa0c32b859824f82271e7724da89d
Jaime Herrera Muñoz <jaimei.herrera@outlook.com>
2022-09-11 14:24:06 -0300

    test

--- src/api.py
+++ src/api.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+import jwt
+
 app = FastAPI()
+
+
+@app.get("/test")
+def test():
+    jwt
+    return None
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to login_ms!"}
---
+++ src/jwt.py
@@ -0,0 +1,5 @@
+import jwt
+
+encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
+print(encoded_jwt)
+print(jwt.decode(encoded_jwt, "secret", algorithms=["HS256"]))
