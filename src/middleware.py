f929c9bbc7cb25176ba85d955dd8fc09d4d3ca47
Jaime Herrera Muñoz <jaimei.herrera@outlook.com>
2022-09-20 19:50:57 -0300

    middleware & log

--- src/middleware.py
+++ src/middleware.py
@@ -1,18 +1,17 @@
+from multiprocessing import Process
+
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from .logger import Log
+
 
 class RequestMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, some_attribute: str):
+    def __init__(self, app):
         super().__init__(app)
-        self.some_attribute = some_attribute
 
     async def dispatch(self, request: Request, call_next):
-        # do something with the request object, for example
-        content_type = request.headers.get('Content-Type')
-        print(content_type)
+        message = "API Requested."
+        Process(target=Log().write_log, args=(message,)).start()
 
-        # process the request and get the response
-        response = await call_next(request)
-
-        return response
+        return await call_next(request)
